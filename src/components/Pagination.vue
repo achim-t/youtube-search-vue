f25d47c95d1a0101cfc9a3ceb6c49462c07d67a5
Achim Taeschner <ataeschner@lebit.net>
2019-11-11 16:46:48 +0100

    finished

---
+++ src/components/Pagination.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <nav>
+            <ul class="pagination justify-content-end">
+                <li class="page-item" v-bind:class="{ disabled: prevPageToken === undefined }">
+                    <a @click="prevPage" href="#" class="page-link">Previous</a>
+                </li>
+                <li class="page-item" v-bind:class="{ disabled: nextPageToken === undefined }">
+                    <a @click="nextPage" href="#" class="page-link">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Pagination',
+    props: ['prevPageToken', 'nextPageToken'],
+    methods: {
+        prevPage() {
+            this.$emit('prev-page')
+        },
+        nextPage() {
+            this.$emit('next-page')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.page-link {
+    box-shadow: none !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import moment from 'moment'
 
+
 Vue.config.productionTip = false
 
 Vue.filter('formatDate', value => {
